ce046795fd11e1c271c3792516256f45b0742b73
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-08 14:44:54 +0100

    Update HostelManagerState

--- Domain/States/Hostel.State/HostelManagerState.cs
+++ Domain/States/Hostel.State/HostelManagerState.cs
@@ -1,31 +1,28 @@
 ﻿using Hostel.Event;
-using Hostel.State.Floor;
+using Hostel.Model;
 using Shared;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Hostel.State
 {
     public class HostelManagerState : Message, IState<HostelManagerState>
     {
-        public IEnumerable<FloorState> FloorStates;
-        public SepticTankState SepticTankState;
-        public WaterReservoirState WaterReservoirState;
+        public Construction ConstructionRecord { get; }
         public bool Constructed;
-        public static readonly HostelManagerState Empty = new HostelManagerState(Enumerable.Empty<FloorState>(), false);
-        public HostelManagerState(IEnumerable<FloorState> floors, bool constructed)
+        public static readonly HostelManagerState Empty = new HostelManagerState(false, null);
+        public HostelManagerState(bool constructed, Construction record)
         {
-            FloorStates = floors;
             Constructed = false;
+            ConstructionRecord = record;
         }
         public HostelManagerState Update(IEvent evnt)
         {
             switch(evnt)
             {
-                case CreatedFloor createdFloor:
+                case ConstructedHostel hostel:
                     {
-                        var floor = createdFloor.Floor;
-                        return new HostelManagerState(FloorStates, true);
+                        var construct = hostel.Construction;
+                        var constructed = !string.IsNullOrWhiteSpace(hostel.Construction.Detail.HostelId);
+                        return new HostelManagerState(constructed, construct);
                     }
                 default: return this;
             }
