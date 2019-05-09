8ae849dae3268d98007a09e4943217c19efa86f8
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-09 11:34:49 +0100

    Install Septic and Reservoir Sensor Stored Procs

---
+++ Database/Hostel/dbo/Stored Procedures/InstallReservoirSensor.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[InstallReservoirSensor]
+	@reservoir uniqueidentifier,
+	@tag nvarchar(50),
+	@role nvarchar(50),
+	@sensorid uniqueidentifier output
+AS
+BEGIN
+	DECLARE @id uniqueidentifier;
+	SET @id = (SELECT ReservoirSensorId FROM Hostel_Water_Reservoir_Sensors WHERE Tag = @tag AND ReservoirId = @reservoir);
+	IF @id IS NULL
+	BEGIN
+		INSERT INTO Hostel_Water_Reservoir_Sensors(ReservoirSensorId, ReservoirId, Tag, [Role])
+		VALUES(@id, @reservoir, @tag, @role)
+		SET @sensorid = @id;
+	END
+	ELSE
+	BEGIN
+		SET @sensorid = @id
+	END
+END
---
+++ Database/Hostel/dbo/Stored Procedures/InstallSepticSensor.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[InstallsEPTICSensor]
+	@septictank uniqueidentifier,
+	@tag nvarchar(50),
+	@role nvarchar(50),
+	@sensorid uniqueidentifier output
+AS
+BEGIN
+	DECLARE @id uniqueidentifier;
+	SET @id = (SELECT SepticSensorId FROM Hostel_Septic_Tank_Sensors WHERE Tag = @tag AND SepticTankId = @septictank);
+	IF @id IS NULL
+	BEGIN
+		INSERT INTO Hostel_Septic_Tank_Sensors(SepticSensorId, SepticTankId, Tag, [Role])
+		VALUES(@id, @septictank, @tag, @role)
+		SET @sensorid = @id;
+	END
+	ELSE
+	BEGIN
+		SET @sensorid = @id
+	END
+END
