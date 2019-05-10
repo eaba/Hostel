c2173069ed6731760ffd370b96d8b86b4a47126e
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-10 19:27:56 +0100

    Database build failure fixes

--- Database/Hostel/dbo/Tables/Hostel_Water_Reservoir_Sensors.sql
+++ Database/Hostel/dbo/Tables/Hostel_Water_Reservoir_Sensors.sql
@@ -5,6 +5,5 @@
     [Tag] NVARCHAR (50)    NOT NULL,
 	[Role] NVARCHAR (50)    NOT NULL,
     CONSTRAINT [PK_Hostel_Water_Reservoir_Sensors] PRIMARY KEY CLUSTERED ([ReservoirSensorId] ASC),
-    CONSTRAINT [FK_Hostel_Water_Reservoir_Sensors_Reservoir] FOREIGN KEY ([ReservoirId]) REFERENCES [dbo].[Hostel_Water_Reservoir_Sensors] ([ReservoirId]),
     CONSTRAINT [IX_Hostel_Water_Reservoir_Sensors] UNIQUE NONCLUSTERED ([Tag] ASC)
 )
