75f1b167747b81225709d6cffa75519e8335d309
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-10 11:28:34 +0100

    Tenancy Tenants

---
+++ Database/Hostel/dbo/Tables/Hostel_Floor_Rooms_Tenancy.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Hostel_Floor_Rooms_Tenancy]
+(
+	[RoomTenancyId]  UNIQUEIDENTIFIER CONSTRAINT [DF_Hostel_Floor_Rooms_Tenancy] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
+    [RoomId] UNIQUEIDENTIFIER NOT NULL,
+    [StartDate] BIGINT    NOT NULL,
+    [EndDate] BIGINT NOT NULL, 
+    [Rate] NVARCHAR(50) NOT NULL, 
+    [PaymentType] NVARCHAR(50) NOT NULL, 
+    [Price] DECIMAL NOT NULL, 
+    CONSTRAINT [PK_Hostel_Floor_Rooms_Tenancy] PRIMARY KEY CLUSTERED ([RoomTenancyId] ASC),
+    CONSTRAINT [FK_Hostel_Floor_Rooms_Tenancy_Hostel_Floor_Rooms] FOREIGN KEY ([RoomId]) REFERENCES [dbo].[Hostel_Floor_Rooms] ([RoomId]),
+    CONSTRAINT [XQ_Hostel_Floor_Rooms_Tenancy] UNIQUE NONCLUSTERED ([RoomId] ASC)
+)
--- Database/Hostel/dbo/Tables/Hostel_Floor_Rooms_Tenants.sql
+++ Database/Hostel/dbo/Tables/Hostel_Floor_Rooms_Tenants.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[Hostel_Floor_Rooms_Tenants]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[TenancyTenantId]  UNIQUEIDENTIFIER CONSTRAINT [DF_Hostel_Floor_Rooms_Tenants] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
+    [PersonId] UNIQUEIDENTIFIER NOT NULL,
+    [RoomTenancyId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Hostel_Floor_Rooms_Tenants] PRIMARY KEY CLUSTERED ([TenancyTenantId] ASC),
+    CONSTRAINT [FK_Tenant_Persons] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Hostel_Persons] ([PersonId]),
+    CONSTRAINT [FK_RoomTenancy] FOREIGN KEY ([RoomTenancyId]) REFERENCES [dbo].[Hostel_Floor_Rooms_Tenancy] ([RoomTenancyId]) ON DELETE CASCADE,
+    CONSTRAINT [Unique_Tenancy_Tenants] UNIQUE NONCLUSTERED ([PersonId] ASC, [RoomTenancyId] ASC)
 )
