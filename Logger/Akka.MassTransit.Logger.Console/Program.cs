ea7e53b893245debd1f1dfec2850910a50e36442
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-10 18:10:22 +0100

    Logg amd Trace Consumers

---
+++ Logger/Akka.MassTransit.Logger.Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using GreenPipes;
+using MassTransit;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Akka.MassTransit.Logger.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+        private static IBusControl CreateBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost:/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                cfg.ReceiveEndpoint(host, "logging_akka", ep =>
+                {
+                    ep.PrefetchCount = 500;
+                    ep.UseRetry(r => r.Immediate(5));
+                    ep.Consumer<LogConsumer>();
+                });
+                cfg.ReceiveEndpoint(host, "trace_akka", ep =>
+                {
+                    ep.PrefetchCount = 500;
+                    ep.UseRetry(r => r.Immediate(5));
+                    ep.Consumer<TraceConsumer>();
+                });
+
+            });
+        }
+        public class LogConsumer : IConsumer<Dto>
+        {
+            public Task Consume(ConsumeContext<Dto> context)
+            {
+                var log = context.Message;
+                System.Console.WriteLine(JsonConvert.SerializeObject(log, Formatting.Indented));
+                return Task.CompletedTask;
+            }
+        }
+        public class TraceConsumer : IConsumer<Dto>
+        {
+            public Task Consume(ConsumeContext<Dto> context)
+            {
+                var log = context.Message;
+                System.Console.WriteLine(JsonConvert.SerializeObject(log, Formatting.Indented));
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
