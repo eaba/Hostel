0cefd70169228b573b7e7a24156d3be2de887b5e
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-17 12:30:41 +0100

    Index

--- Presentation/Web/Landing/Hostel.Web.Landing.Angular/src/app/app.component.ts
+++ Presentation/Web/Landing/Hostel.Web.Landing.Angular/src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-//import { slideInAnimation } from './components/home/home.animation';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    //slideInAnimation
-    // animation triggers go here
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-  }
+  title = 'Hostel';
 }
