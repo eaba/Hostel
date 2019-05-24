291a55f456eae8b82bfc885692d7781eb2bf2c34
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-24 17:40:50 +0100

    QueryActor

--- Domain/Queries/Hostel.Query/ManagerQueryActor.cs
+++ Domain/Queries/Hostel.Query/ManagerQueryActor.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Akka.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Hostel.Query
 {
-    class ManagerQueryActor
+    public class ManagerQueryActor : ReceivePersistentActor
     {
+        public override string PersistenceId { get; }
+        public ManagerQueryActor(string persistenceId)
+        {
+            PersistenceId = Context.Parent.Path.Name+"_Query";
+        }
     }
 }
