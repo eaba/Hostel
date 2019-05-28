32aca209b074adb1d5aeda65ac70ae3bfc411a49
Ebere Abanonu <eabanonu@yahoo.com>
2019-05-28 11:53:46 +0100

    Hostel Query

---
+++ Database/Hostel.Query/dbo/Tables/Hostel_Tenants.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Hostel_Tenants]--https://docs.microsoft.com/en-us/sql/relational-databases/json/store-json-documents-in-sql-tables?view=sql-server-2017
+(
+	[HostelTenantsId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Hostel] UNIQUEIDENTIFIER NOT NULL, 
+    [Tenants] NVARCHAR(MAX) NOT NULL,
+	INDEX cci CLUSTERED COLUMNSTORE ---If you expect to have a large number of JSON documents in your collection, we recommend adding a CLUSTERED COLUMNSTORE index on the collection
+)
